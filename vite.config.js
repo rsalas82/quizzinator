1ebbcae6c100f84ee735d8158afceb675a664707
Rafa Salas <rsalas@viafirma.com>
2022-03-21 17:53:47 +0100

    Add simple test

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    globals: true,
+    setupFiles: 'src/Test/setupTests.js',
+    environment: 'jsdom',
+  },
 })
